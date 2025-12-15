32abd288d1b3073b9bd76c6d04bf83aab16f59e9
Michal Chvatal <michal.chvatal@seznam.cz>
2025-12-15 13:54:09 +0100

    zmena emailu
    Signed-off-by: Michal Chvatal <michal.chvatal@seznam.cz>

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ All media content (images, video, logo) are NOT covered by the MIT license and r
 document.addEventListener('DOMContentLoaded', () => {
     // CONFIGURATION: Paste your Google Apps Script Web App URL here
     // See google_sheets_setup.md for instructions
-    const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxZh32MqLE6CXNf8fA9VqRpelvX6E4IeT5PDSTDAsmtVK4kJXNKIaBJUK_N5at68ED-/exec';
+    const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwi_0LNKoeL3C3ngUZfCo-KMWQ6hHNicS0Q05RuKzpcjquzE93KEpp7aLON2zLkb3MV/exec';
 
     // Mobile Navigation Toggle
     const toggleBtn = document.querySelector('.mobile-menu-toggle');
